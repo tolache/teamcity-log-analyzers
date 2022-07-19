083ae08eaed1f37fac8d268a7ce6cd00c7d799a7
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2022-07-19 16:33:33 +0200

    list vcs roots sorted by polling duration

--- avg-polling-time.ps1
+++ avg-polling-time.ps1
@@ -2,9 +2,7 @@ $logsFolder = "C:\Users\Anatoly.Cherenkov\Downloads\4171080\teamcity_server_logs
 $vcsRootCount = 10709
 $pollerThreadCount = 10
 
-# Calculate average VCS polling time
-
-function Get-PollingTime {
+function Get-PollingDuration {
     param (
         [Parameter(Position=0)]
         [string]
@@ -13,9 +11,9 @@ function Get-PollingTime {
 
     if ($InputLogLine -notmatch ", total time: (?<content>.*), persisting time: ")
     {
-        return "Log line doesn't contain polling time"
+        return "Log line doesn't contain polling duration"
     }
-    
+
     $timeString = $matches['content']
     $timeStringArray = $timeString -split ","
 
@@ -25,7 +23,7 @@ function Get-PollingTime {
     {
         $amount = ($element -split '\D')[0]
         $units = ($element -split '\d')[-1]
-        
+
         if ($units -eq "h")
         {
             $timespan = $timespan.Add([TimeSpan]::FromHours($amount))
@@ -51,17 +49,81 @@ function Get-PollingTime {
     return $timeSpan
 }
 
-# Calculate and show stats
-[TimeSpan]$totalTime = [TimeSpan]::FromMilliseconds(0)
-[Int32]$successfulPolls = 0
-foreach ($line in Get-Content $logsFolder/teamcity-vcs.log*) {
-    $lineParseResult = Get-PollingTime $line
+function Get-VcsName {
+    param (
+        [Parameter(Position = 0)]
+        [string]
+        $InputLogLine
+    )
+
+    if ($InputLogLine -notmatch 'Finish collecting changes successfully (for|from) VCS root "(?<content>.*?)" {instance id=')
+    {
+        $InputLogLine > C:\Temp\debug.log
+        throw "Log line doesn't contain a VCS name: $InputLogLine"
+    }
+
+    return $matches['content']
+}
+
+function Get-PollingTime {
+    param (
+        [Parameter(Position = 0)]
+        [string]
+        $InputLogLine
+    )
+
+    if ($InputLogLine -notmatch "\[(?<content>.*)\]   INFO \[")
+    {
+        return "Log line doesn't contain polling time"
+    }
+
+    $timeString = $matches['content']
+    [DateTime]$timestamp = [Datetime]::ParseExact($timeString, 'yyyy-MM-dd HH:mm:ss,fff', $null)
+    return $timestamp
+}
+
+class PollingAttempt
+{
+    [ValidateNotNullOrEmpty()][DateTime]$Time
+    [ValidateNotNullOrEmpty()][string]$VcsName
+    [ValidateNotNullOrEmpty()][TimeSpan]$Duration
+    [ValidateNotNullOrEmpty()][string]$RawLogLine
     
-    if ($lineParseResult.GetType().Name -eq "TimeSpan") {
-        $totalTime = $totalTime.Add($lineParseResult)
-        $successfulPolls++
+    PollingAttempt($Time, $VcsName, $Duration, $RawLogLine) {
+        $this.Time = $Time
+        $this.VcsName = $VcsName
+        $this.Duration = $Duration
+        $this.RawLogLine = $RawLogLine
     }
 }
+
+# Calculate and show stats
+$pollingAttempts = [System.Collections.ArrayList]::new() 
+
+[TimeSpan]$totalTime = [TimeSpan]::FromMilliseconds(0)
+[Int32]$successfulPolls = 0
+
+foreach ($line in Get-Content $logsFolder/teamcity-vcs.log*) {
+    $currentPollDuration = Get-PollingDuration $line
+    
+    if ($currentPollDuration.GetType().Name -eq "TimeSpan") {
+        $totalTime = $totalTime.Add($currentPollDuration)
+        $successfulPolls++
+
+        $time = Get-PollingTime $line
+        $vcsName = Get-VcsName $line
+        $currentPollingAttempt = [PollingAttempt]::new(
+            $time,
+            $vcsName,
+            $currentPollDuration,
+            $line
+        )
+        $pollingAttempts += $currentPollingAttempt
+    }
+}
+    
+## Show general statistics
+
 [TimeSpan]$avgPollTime = $totalTime / $successfulPolls
 [TimeSpan]$suggestedPollingInterval = $avgPollTime * $vcsRootCount / $pollerThreadCount
 
@@ -69,3 +131,10 @@ foreach ($line in Get-Content $logsFolder/teamcity-vcs.log*) {
 "Sucessful polls: $successfulPolls"
 "Average polling time: $avgPollTime"
 "It will take $suggestedPollingInterval to poll $vcsRootCount VCS Roots in $pollerThreadCount pollerThreadCount$pollerThreadCount"
+
+## Show longest polling attempts
+
+$sortedPolls = $pollingAttempts | Sort-Object -Property Duration -Descending
+foreach ($poll in $sortedPolls) {
+    Write-Output $poll
+}
